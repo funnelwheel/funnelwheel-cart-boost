011953215aca3a8623358fde51fec23d821587d9
Bhoot Biswas <bhoot.biswas@gmail.com>
2021-12-27 15:07:19 +0600

    Add initial files

---
+++ woocommerce-sticky-cart.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Woocommerce Sticky Cart
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     woocommerce-sticky-cart
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Woocommerce_Sticky_Cart
+ */
+
+// Your code starts here.
