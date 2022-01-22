9e7949bca3932fa80023587595043136084e4ce7
Bhoot Biswas <bhoot.biswas@gmail.com>
2022-01-22 15:24:49 +0600

    Update get_cart_information()

--- includes/class-woocommerce-grow-cart-ajax.php
+++ includes/class-woocommerce-grow-cart-ajax.php
@@ -19,8 +19,16 @@ class WooCommerce_Grow_Cart_Ajax {
 	public function get_cart_information() {
 		wp_send_json(
 			[
-				'is_empty' => WC()->cart->is_empty(),
-				'items'    => get_cart_items(),
+				'is_empty'            => WC()->cart->is_empty(),
+				'items'               => get_cart_items(),
+				'tax_enabled'         => wc_tax_enabled(),
+				'shipping_enabled'    => WC()->cart->needs_shipping() && WC()->cart->show_shipping(),
+				'discount_enabled'    => WC()->cart->has_discount(),
+				'cart_subtotal'       => WC()->cart->get_cart_subtotal(),
+				'cart_tax'            => WC()->cart->get_cart_tax(),
+				'cart_shipping_total' => WC()->cart->get_cart_shipping_total(),
+				'cart_discount_total' => WC()->cart->get_cart_discount_total(),
+				'total'               => WC()->cart->get_total(),
 			]
 		);
 	}
@@ -30,16 +38,7 @@ class WooCommerce_Grow_Cart_Ajax {
 		$quantity = (int) $_POST['quantity'];
 
 		if ( ! is_numeric( $quantity ) || $quantity < 0 || ! $cart_key ) {
-			wp_send_json(
-				[
-					'error'     => __( 'Something went wrong' ),
-					'$cart_key' => $cart_key,
-					'$quantity' => $quantity,
-					'$_POST' => $_POST,
-					'$_GET' => $_GET
-
-				]
-			);
+			wp_send_json( [ 'error' => __( 'Something went wrong' ) ] );
 		}
 
 		$cart_success = 0 === $quantity ? WC()->cart->remove_cart_item( $cart_key ) : WC()->cart->set_quantity( $cart_key, $quantity );
