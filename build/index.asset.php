483ac8e0081730c998099bedb3e1e609ab9e9fb8
Bhoot Biswas <bhoot.biswas@gmail.com>
2022-01-23 14:22:25 +0600

    Build assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'fb00adb958b2005b2b0297f69e364a58');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '126eca8ac3d654b5ac3a6b2d77a5decd');
