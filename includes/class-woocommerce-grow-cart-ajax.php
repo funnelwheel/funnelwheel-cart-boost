88d5182f44147de43da4f1ef0dd5ed1a27e6aa98
Bhoot Biswas <bhoot.biswas@gmail.com>
2022-01-24 14:22:11 +0600

    Add get_suggested_products()

--- includes/class-woocommerce-grow-cart-ajax.php
+++ includes/class-woocommerce-grow-cart-ajax.php
@@ -1,6 +1,8 @@
 <?php
 namespace Upnrunn;
 
+use WP_Query;
+
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
@@ -14,6 +16,8 @@ class WooCommerce_Grow_Cart_Ajax {
 		add_action( 'wp_ajax_nopriv_growcart_get_cart_information', [ $this, 'get_cart_information' ] );
 		add_action( 'wp_ajax_growcart_update_cart_item', [ $this, 'update_cart_item' ] );
 		add_action( 'wp_ajax_nopriv_growcart_update_cart_item', [ $this, 'update_cart_item' ] );
+		add_action( 'wp_ajax_growcart_get_suggested_products', [ $this, 'get_suggested_products' ] );
+		add_action( 'wp_ajax_nopriv_growcart_get_suggested_products', [ $this, 'get_suggested_products' ] );
 	}
 
 	public function get_cart_information() {
@@ -50,5 +54,58 @@ class WooCommerce_Grow_Cart_Ajax {
 		];
 
 		wp_send_json( $response );
+	}
+
+	public function get_suggested_products() {
+		global $post;
+
+		$suggested_products = [];
+		$max_items          = 5;
+		$exclude_ids        = [];
+		$cart               = WC()->cart->get_cart();
+		$cart_is_empty      = WC()->cart->is_empty();
+
+		if ( $cart_is_empty ) {
+			$args = array(
+				'post_type'           => 'product',
+				'post_status'         => 'publish',
+				'ignore_sticky_posts' => true,
+				'meta_key'            => 'total_sales',
+				'order'               => 'DESC',
+				'orderby'             => 'meta_value_num',
+				'fields'              => 'ids',
+			);
+
+			$query = new WP_Query( $args );
+
+			$suggested_products = array_merge( $suggested_products, wp_parse_id_list( $query->posts ) );
+		} else {
+			foreach ( $cart as $cart_item ) {
+				$exclude_ids[] = $cart_item['product_id'];
+
+				if ( count( $suggested_products ) >= $max_items ) {
+					continue;
+				}
+
+				$product_id         = $cart_item['variation_id'] ? $cart_item['variation_id'] : $cart_item['product_id'];
+				$related_products   = wc_get_related_products( $product_id, $max_items, $exclude_ids );
+				$suggested_products = array_merge( $suggested_products, wp_parse_id_list( $related_products ) );
+			}
+		}
+
+		$return = [];
+
+		foreach ( $suggested_products as $product_id ) {
+			$_product = wc_get_product( $product_id );
+
+			$return[] = [
+				'product_title'     => $_product->get_title(),
+				'product_permalink' => $_product->is_visible() ? $_product->get_permalink( $cart_item ) : '',
+				'product_thumbnail' => $_product->get_image(),
+				'product_price'     => WC()->cart->get_product_price( $_product ),
+			];
+		}
+
+		wp_send_json( $return );
 	}
 }
