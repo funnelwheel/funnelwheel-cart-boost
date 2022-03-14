137a696548f287888f246549d964317642aed85f
Bhoot Biswas <bhoot.biswas@gmail.com>
2022-03-14 13:25:29 +0600

    Fix responsive issues

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-element'), 'version' => '42d7c0752209b20120f824e19c6c2bb3');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-element'), 'version' => '18934ae8ccc948250937b52f766cb33c');
