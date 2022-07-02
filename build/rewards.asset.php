5f1dba6427c1352d77fc2ed87ba3012b2971ae03
Mithun Biswas <bhoot.biswas@gmail.com>
2022-07-02 09:49:07 +0600

    Build assets

--- build/rewards.asset.php
+++ build/rewards.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '200c58298b2989f7adc0');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd56ba1c2bf847d449b68');
