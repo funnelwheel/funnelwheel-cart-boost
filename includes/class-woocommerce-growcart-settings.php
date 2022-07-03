a632ab664faa81aba5f96026c4833bbe0074f51b
Mithun Biswas <bhoot.biswas@gmail.com>
2022-07-03 16:22:50 +0600

    Add deactivate_license()

--- includes/class-woocommerce-growcart-settings.php
+++ includes/class-woocommerce-growcart-settings.php
@@ -42,6 +42,7 @@ class WooCommerce_Growcart_Settings {
 	public function __construct() {
 		add_action('init', [$this, 'init']);
 		add_action('admin_init', [$this, 'activate_license']);
+		add_action('admin_init', [$this, 'deactivate_license']);
 		add_action('admin_notices', [$this, 'admin_notices']);
 		add_action('admin_init', [$this, 'register_settings']);
 		add_action('admin_menu', array($this, 'add_plugin_pages'));
@@ -137,7 +138,7 @@ class WooCommerce_Growcart_Settings {
 
 						case 'item_name_mismatch':
 
-							$message = sprintf(__('This appears to be an invalid license key for %s.'), EDD_SAMPLE_ITEM_NAME);
+							$message = sprintf(__('This appears to be an invalid license key for %s.'), WOOCOMMERCE_GROWCART_STORE_ITEM_NAME);
 							break;
 
 						case 'no_activations_left':
@@ -165,6 +166,62 @@ class WooCommerce_Growcart_Settings {
 			// $license_data->license will be either "valid" or "invalid"
 
 			update_option('edd_growcart_license_status', $license_data->license);
+			wp_redirect(admin_url('admin.php?page=' . WOOCOMMERCE_GROWCART_LICENSE_PAGE));
+			exit();
+		}
+	}
+
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
+	public function deactivate_license() {
+		// listen for our activate button to be clicked
+		if (isset($_POST['edd_license_deactivate'])) {
+			// run a quick security check
+			if (!check_admin_referer('edd_growcart_license_nonce', 'edd_growcart_license_nonce'))
+				return; // get out if we didn't click the Activate button
+
+			// retrieve the license from the database
+			$license = trim(get_option('edd_growcart_license_key'));
+
+
+			// data to send in our API request
+			$api_params = array(
+				'edd_action' => 'deactivate_license',
+				'license'    => $license,
+				'item_name'  => urlencode(WOOCOMMERCE_GROWCART_STORE_ITEM_NAME), // the name of our product in EDD
+				'url'        => home_url()
+			);
+
+			// Call the custom API.
+			$response = wp_remote_post(WOOCOMMERCE_GROWCART_STORE_URL, array('timeout' => 15, 'sslverify' => false, 'body' => $api_params));
+
+			// make sure the response came back okay
+			if (is_wp_error($response) || 200 !== wp_remote_retrieve_response_code($response)) {
+
+				if (is_wp_error($response)) {
+					$message = $response->get_error_message();
+				} else {
+					$message = __('An error occurred, please try again.');
+				}
+
+				$base_url = admin_url('admin.php?page=' . WOOCOMMERCE_GROWCART_LICENSE_PAGE);
+				$redirect = add_query_arg(array('sl_activation' => 'false', 'message' => urlencode($message)), $base_url);
+
+				wp_redirect($redirect);
+				exit();
+			}
+
+			// decode the license data
+			$license_data = json_decode(wp_remote_retrieve_body($response));
+
+			// $license_data->license will be either "deactivated" or "failed"
+			if ($license_data->license == 'deactivated') {
+				delete_option('edd_growcart_license_status');
+			}
+
 			wp_redirect(admin_url('admin.php?page=' . WOOCOMMERCE_GROWCART_LICENSE_PAGE));
 			exit();
 		}
