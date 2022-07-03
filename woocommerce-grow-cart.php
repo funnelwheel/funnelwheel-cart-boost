f339adb9551fd08dc2619f3e086e40a7288659b0
Mithun Biswas <bhoot.biswas@gmail.com>
2022-07-03 13:53:56 +0600

    Update WOOCOMMERCE_GROWCART_STORE_ITEM_ID

--- woocommerce-grow-cart.php
+++ woocommerce-grow-cart.php
@@ -17,7 +17,7 @@ defined( 'ABSPATH' ) || exit;
 
 define( 'WOOCOMMERCE_GROWCART_FILE', __FILE__ );
 define( 'WOOCOMMERCE_GROWCART_STORE_URL', 'https://wpdrift.com' );
-define( 'WOOCOMMERCE_GROWCART_STORE_ITEM_ID', 123 );
+define( 'WOOCOMMERCE_GROWCART_STORE_ITEM_ID', 304 );
 define( 'WOOCOMMERCE_GROWCART_STORE_ITEM_NAME', __('GrowCart for WooCommerce') );
 define( 'WOOCOMMERCE_GROWCART_LICENSE_PAGE', 'growcart-license' );
 
