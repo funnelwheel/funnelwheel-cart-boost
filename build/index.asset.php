37beba7e9dcc401d46725f25f079c650d1680ec6
Mithun Biswas <bhoot.biswas@gmail.com>
2022-07-06 15:12:18 +0600

    Build assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-element'), 'version' => '734f361252d5ac2cc24d');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-element'), 'version' => '366ab3f4b622851d2614');
--- build/rewards.asset.php
+++ build/rewards.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '947308fef9a6a68a9664');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cf35c099f65dff6e0d79');
