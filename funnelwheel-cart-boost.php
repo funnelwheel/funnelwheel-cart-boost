abd719670a20650bc2010effec10fcc998357182
Kishore Chandra Sahoo <kishore@upnrunn.com>
2025-08-18 08:35:39 +0530

    Update funnelwheel-cart-boost.php

--- funnelwheel-cart-boost.php
+++ funnelwheel-cart-boost.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * Plugin Name:     GrowCart for WooCommerce
- * Plugin URI:      https://wpdrift.com/growcart/
- * Description:     GrowCart for WooCommerce delivers mobile-first, CRO-optimized site experiences that get you more customers, that spend more and subscribe longer.
- * Author:          WPdrift
- * Author URI:      https://wpdrift.com/
- * Text Domain:     woocommerce-growcart
+ * Plugin Name:     Funnelwheel Cart Boost
+ * Plugin URI:      http://funnelwheel.com/cart-boost
+ * Description:     Cart Boost for WooCommerce delivers mobile-first, CRO-optimized site experiences that get you more customers, that spend more and subscribe longer.
+ * Author:          Funnelwheel
+ * Author URI:      http://funnelwheel.com
+ * Text Domain:     funnelwheel-cart-boost
  * Domain Path:     /languages
  * Version:         0.1.0
  *
- * @package         WooCommerce_GrowCart
+ * @package         Funnelwheel_Cart_Boost
  */
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
 define( 'WOOCOMMERCE_GROWCART_FILE', __FILE__ );
-define( 'WOOCOMMERCE_GROWCART_STORE_URL', 'https://wpdrift.com' );
+define( 'WOOCOMMERCE_GROWCART_STORE_URL', 'http://funnelwheel.com' );
 define( 'WOOCOMMERCE_GROWCART_STORE_ITEM_ID', 304 );
 define( 'WOOCOMMERCE_GROWCART_STORE_ITEM_NAME', __( 'GrowCart for WooCommerce', 'woocommerce-growcart' ) );
 define( 'WOOCOMMERCE_GROWCART_LICENSE_PAGE', 'growcart-license' );
